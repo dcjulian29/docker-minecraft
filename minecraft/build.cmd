3267fe9444617d7685871d3dba014f88354fdbf8
Julian Easterling <julian@julianscorner.com>
2021-01-07 19:27:49 -0500

    Fixed my "docker push" to push all tags with images

--- minecraft/build.cmd
+++ minecraft/build.cmd
@@ -1,3 +1,3 @@
 docker build -t dcjulian29/minecraft:1.16.4 .
 docker tag dcjulian29/minecraft:1.16.4 dcjulian29/minecraft:latest
-docker push -a dcjulian29/minecraft
+docker push dcjulian29/minecraft --all-tags

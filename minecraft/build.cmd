3cd4022f24dad497538cdaeb6031e29684f43542
Julian Easterling <julian@julianscorner.com>
2021-05-25 16:09:34 -0400

    Added BungeeCord Minecraft proxy container image

--- minecraft/build.cmd
+++ minecraft/build.cmd
@@ -7,4 +7,7 @@ docker tag dcjulian29/minecraft:1.16.5-vanilla dcjulian29/minecraft:latest-vanil
 docker build -t dcjulian29/minecraft:1.16.221-bedrock -f Dockerfile-Bedrock .
 docker tag dcjulian29/minecraft:1.16.221-bedrock dcjulian29/minecraft:latest-bedrock
 
+docker build -t dcjulian29/minecraft:1.16.5-bungeecord -f Dockerfile-BungeeCord .
+docker tag dcjulian29/minecraft:1.16.5-bungeecord dcjulian29/minecraft:latest-bungeecord
+
 docker push dcjulian29/minecraft --all-tags

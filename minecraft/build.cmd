3ccf5c1f965835cb2b15c9e29a9146f61d9a437f
Julian Easterling <julian@julianscorner.com>
2021-07-04 19:51:05 -0400

    The Vanilla, Bedrock, and BungeeCord images should not use cached layers during build. Also added some nice output

--- minecraft/build.cmd
+++ minecraft/build.cmd
@@ -6,26 +6,56 @@ set VERSION=1.17.0
 set BEDROCK=1.17.2.01
 set BUNGEECORD=1.5.85
 
-docker build --progress plain -t dcjulian29/minecraft:%VERSION% .
+echo.
+echo *
+echo * Vanilla Build (%VERSION%)
+echo *
+echo.
+
+docker build --no-cache --progress plain -t dcjulian29/minecraft:%VERSION% .
 docker tag dcjulian29/minecraft:%VERSION% dcjulian29/minecraft:latest
 
 pushd paper
+echo.
+echo *
+echo * Paper Build (%VERSION%)
+echo *
+echo.
+
 docker build --progress plain -t dcjulian29/minecraft:%VERSION%-paper .
 docker tag dcjulian29/minecraft:%VERSION%-paper dcjulian29/minecraft:latest-paper
 popd
 
 pushd spigot
+echo.
+echo *
+echo * Spigot Build (%VERSION%)
+echo *
+echo.
+
 docker build --progress plain -t dcjulian29/minecraft:%VERSION%-spigot .
 docker tag dcjulian29/minecraft:%VERSION%-spigot dcjulian29/minecraft:latest-spigot
 popd
 
 pushd bedrock
-docker build --progress plain -t dcjulian29/bedrock:%BEDROCK% .
+echo.
+echo *
+echo * Bedrock Build (%BEDROCK%)
+echo *
+echo.
+
+docker build --no-cache --progress plain -t dcjulian29/bedrock:%BEDROCK% .
 docker tag dcjulian29/bedrock:%BEDROCK% dcjulian29/bedrock:latest
 popd
 
 pushd bungeecord
-docker build --progress plain -t dcjulian29/bungeecord:%BUNGEECORD% .
+echo.
+echo *
+echo * BungeeCord Build (%BUNGEECORD%)
+echo *
+echo.
+
+docker build --no-cache --progress plain -t dcjulian29/bungeecord:%BUNGEECORD% .
 docker tag dcjulian29/bungeecord:%BUNGEECORD% dcjulian29/bungeecord:latest
 popd
 
